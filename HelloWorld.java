8543551f68f19cef3f3bc4d4da7c917bdb89cc6d
fromtoc <f17932486@gmail.com>
2020-09-04 14:15:38 +0800

    v2

--- HelloWorld.java
+++ HelloWorld.java
@@ -6,7 +6,7 @@ import javax.servlet.http.*;
 
 public class HelloWorld extends HttpServlet {
 	
-	int count = 0;
+	int count = 1;
 
 	public void doGet(HttpServletRequest req, HttpServletResponse res)
 			                         throws ServletException, IOException {
